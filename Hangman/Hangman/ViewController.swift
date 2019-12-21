3660a6932deb0033bbdfe6d7e544def29fd57058
Topher Aronson <christopheraronson@icloud.com>
2019-12-20 17:26:17 -0800

    Created the Hangman project

---
+++ Hangman/Hangman/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Hangman
+//
+//  Created by Christopher Aronson on 12/20/19.
+//  Copyright © 2019 Christopher Aronson. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
